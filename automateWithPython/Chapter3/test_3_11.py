66b12577fd022cf91f585c8f472b93a9b6c28a06
sniper1211 <busivaa@gmail.com>
2018-03-13 15:58:06 +0800

    collatz, amazing

--- automateWithPython/Chapter3/test_3_11.py
+++ automateWithPython/Chapter3/test_3_11.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+print('Enter a number')
+number = int(input())
+
+def collatz(number):
+    while number != 1:
+        if number % 2 == 0:
+            number = number // 2
+            print(number)
+        elif number % 2 == 1:
+            number = number*3+1
+            print(number)
+
+collatz(number)
