a849ae665430d0b2685565a4b3870437c60193d8
sniper1211 <busivaa@gmail.com>
2018-03-18 01:16:23 +0800

    now i know how to printTable

--- automateWithPython/Chapter6/printTable.py
+++ automateWithPython/Chapter6/printTable.py
@@ -8,11 +8,18 @@ tableData = [
 ]
 
 def printTable(tableData):
+    #生成一个列表,包含三个子列表的长度,初始表示为0
     colWidths = [0] * len(tableData)
+
+    #遍历第一个子列表的中每一项
     for y in range(len(tableData[0])):
+        #遍历每个子列表
         for x in range(len(tableData)):
+            #如果子列表的长度 小于 子列表内元素的长度
             if colWidths[x] < len(tableData[x][y]):
+                #就取较大值
                 colWidths[x] = len(tableData[x][y])
+
     for y in range(len(tableData[0])):
         for x in range(len(tableData)):
             if x == 0:
