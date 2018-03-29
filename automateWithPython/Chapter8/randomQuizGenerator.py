e921de7f71e98404c256228ad345468f4cef8de8
sniper1211 <busivaa@gmail.com>
2018-03-29 16:11:00 +0800

    nothing

--- automateWithPython/Chapter8/randomQuizGenerator.py
+++ automateWithPython/Chapter8/randomQuizGenerator.py
@@ -27,7 +27,7 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
 for quizNum in range(35):
     # Create the quiz and answer key files.
     quizFile = open('capitalsquiz%s.txt' % (quizNum + 1), 'w')
-    answerKeyFile = open('capitalsquiz_answers&s.txt' % (quizNum + 1), 'w')
+    answerKeyFile = open('capitalsquiz_answers%s.txt' % (quizNum + 1), 'w')
 
     # Write out the header for the quiz.
     quizFile.write('Name:\n\nDate:]n]nPeriod:\n\n')
@@ -48,3 +48,12 @@ for quizNum in range(35):
         random.shuffle(answerOptions)
 
         # Write the question and the answer options to the quiz file.
+        quizFile.write('%s. What is the capital of %s?\n' % (questionNum + 1, states[questionNum]))
+        for i in range(4):
+            quizFile.write(' %s. %s\n' % ('ABCD'[i], answerOptions[i]))
+        quizFile.write('\n')
+
+        # Write the answer key to a file.
+        answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
+    quizFile.close()
+    answerKeyFile.close()
