b4a90a02a95de717d865881b49845551adf8ecab
sniper1211 <busivaa@gmail.com>
2018-04-13 13:48:45 +0800

    continue 9.4 on step 2

--- automateWithPython/Chapter9/renameDates.py
+++ automateWithPython/Chapter9/renameDates.py
@@ -4,14 +4,26 @@
 import shutil, os, re
 
 # Create a regex that matches files with the American date format.
-dataPattern = re.compile(r"""^(.*?) # all text before the date
-((0|1)?\d) -        # one or two digits for the month
-((0|1|2|3)?\d) -    # one or two digits for the day
-((19|20)?\d\d) -    # four digits for the year
+datePattern = re.compile(r"""^(.*?) # all text before the date
+((0|1)?\d)-        # one or two digits for the month
+((0|1|2|3)?\d)-    # one or two digits for the day
+((19|20)?\d\d)-    # four digits for the year
 (.*?)$              # all text after the date
 """, re.VERBOSE)
 
-# TODO: Loop over the files in the working directory.
+# Loop over the files in the working directory.
+for amerFilename in os.listdir('.'):
+    mo = datePattern.search(amerFilename)
+
+    # Skip files without a date.
+    if mo == None:
+        continue
+    # Get the different parts of the filename.
+    beforePart = mo.groups(1)
+    monthPart = mo.groups(2)
+    dayPart = mo.groups(4)
+    yearPart = mo.groups(6)
+    afterPart = mo.groups(8)
 
 # TODO: Skip files without a date.
 
