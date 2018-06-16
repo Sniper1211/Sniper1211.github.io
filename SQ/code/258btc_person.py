0d265993db60aff818b1c43f626afdb41b5dda94
sniper1211 <lxx328825321@sina.com>
2018-06-16 17:07:07 +0800

    spider bolder

--- SQ/code/258btc_person.py
+++ SQ/code/258btc_person.py
@@ -1,18 +1,29 @@
 # -*- coding: utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
+import time, random
 
-pageNumb = '2797'
+# pageNum = '2797'
 # 目前探明有效页码范围： 2415 - 3011
-resp = requests.get('http://www.528btc.com/person/'+ pageNumb +'.html')
 
-soup = BeautifulSoup(resp.content, 'html.parser')
+def getInfo(rawPageNum):
+    pageNum = str(rawPageNum)
+    resp = requests.get('http://www.528btc.com/person/'+ pageNum +'.html')
 
-name = soup.select('.name.marr10')[0].get_text()
-guest_info = soup.select('.guest_info')[0].get_text()
-img = soup.select('div.sidebar.right > div:nth-of-type(2) > img')[0].get('src')
+    soup = BeautifulSoup(resp.content, 'html.parser')
+
+    name = soup.select('.name.marr10')[0].get_text()
+    guest_info = soup.select('.guest_info')[0].get_text()
+    img = soup.select('div.sidebar.right > div:nth-of-type(2) > img')[0].get('src')
+
+    print(name)
+    print(guest_info)
+    print(img)
 
 
-print(name)
-print(guest_info)
-print(img)
+for i in range(2433, 3011):
+    print('*'*20)
+    print(i)
+    getInfo(i)
+    # sleepTime = random.randint(1,10)
+    # time.sleep(sleepTime)
