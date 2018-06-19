da2887d1e2795a0f0e9adda06c1d71c33528e540
sniper1211 <lxx328825321@sina.com>
2018-06-19 16:27:21 +0800

    爬取币界网

--- SQ/code/258btc_person.py
+++ SQ/code/258btc_person.py
@@ -1,29 +1,39 @@
 # -*- coding: utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
-import time, random
+import time, random, csv
 
 # pageNum = '2797'
 # 目前探明有效页码范围： 2415 - 3011
 
 def getInfo(rawPageNum):
-    pageNum = str(rawPageNum)
-    resp = requests.get('http://www.528btc.com/person/'+ pageNum +'.html')
+    try:
+        pageNum = str(rawPageNum)
+        resp = requests.get('http://www.528btc.com/person/'+ pageNum +'.html')
 
-    soup = BeautifulSoup(resp.content, 'html.parser')
+        soup = BeautifulSoup(resp.content, 'html.parser')
 
-    name = soup.select('.name.marr10')[0].get_text()
-    guest_info = soup.select('.guest_info')[0].get_text()
-    img = soup.select('div.sidebar.right > div:nth-of-type(2) > img')[0].get('src')
+        name = soup.select('.name.marr10')[0].get_text()
+        guest_info = soup.select('.guest_info')[0].get_text()
+        img = soup.select('div.sidebar.right > div:nth-of-type(2) > img')[0].get('src')
+        # 打印 人名、简介、照片
+        print(name)
+        print(guest_info)
+        print(img)
 
-    print(name)
-    print(guest_info)
-    print(img)
+        with open('/Users/lisinai/coding/sniper1211.github.io/SQ/result/data.csv', 'a') as csvfile:
+            writer = csv.writer(csvfile)
+            # writer.writerow(['id', 'name','description', 'img'])
+            writer.writerow([pageNum, name, guest_info, img])
+    except IndexError:
+        with open('/Users/lisinai/coding/sniper1211.github.io/SQ/result/data.csv', 'a') as csvfile:
+            writer = csv.writer(csvfile)
+            # writer.writerow(['id', 'name','description', 'img'])
+            writer.writerow([pageNum, '😝', '😝', '😝'])
 
 
-for i in range(2433, 3011):
+
+for i in range(2415, 3012):
     print('*'*20)
     print(i)
     getInfo(i)
-    # sleepTime = random.randint(1,10)
-    # time.sleep(sleepTime)
