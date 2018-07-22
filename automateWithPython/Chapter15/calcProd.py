1dd1d8baa0905b9d32127f7775a105dc9e0d3323
sniper1211 <busivaa@gmail.com>
2018-07-22 23:58:26 +0800

    update.

---
+++ automateWithPython/Chapter15/calcProd.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import time
+
+def calcProd():
+    # Calculate the product of the first 100,000 numbers.
+    product = 1
+    for i in range(1,10000):
+        product *= i
+    return product
+
+startTime = time.time()
+prod = calcProd()
+endTime = time.time()
+print('The result is %s digits long.' % (len(str(prod))))
+print('Took %s seconds to calculate.' % (endTime - startTime))
