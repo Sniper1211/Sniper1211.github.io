25ec124c1b92f403e752a7320dde7b829d1f270d
sniper1211 <busivaa@gmail.com>
2018-11-01 10:11:04 +0800

    update.

--- ranking/getData.py
+++ ranking/getData.py
@@ -4,3 +4,15 @@
 import requests
 from bs4 import BeautifulSoup
 
+# 目标 url
+url = "http://insight.baidu.com/base/search/rank/list?pageSize=20&source=0&toFixed=1&filterType=1&dateType=20181015~20181021&dimensionid=78&rateType=1000&filterNodes=23871%7C80&callback=_jsonp5xigt119is"
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
+# 发起请求
+re = requests.get(url, headers=headers)
+
+# Unicode 转换成中文
+decoded = re.text.encode('latin-1').decode('unicode_escape')
+
+soup = BeautifulSoup(decoded, 'html.parser')
+print(soup)
