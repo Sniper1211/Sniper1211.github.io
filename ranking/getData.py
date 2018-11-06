f9af4ed04cbd6c9374dc09cd3a0d513712682692
sniper1211 <busivaa@gmail.com>
2018-11-07 00:10:56 +0800

    get weeks data.

--- ranking/getData.py
+++ ranking/getData.py
@@ -3,13 +3,14 @@
 
 import requests
 from bs4 import BeautifulSoup
+import time
 
 
 
 # 目标 url
-url = "http://insight.baidu.com/base/search/rank/list?pageSize=20&source=0&toFixed=1&filterType=1&dateType=20181015~20181021&dimensionid=78&rateType=1000"
-# &filterNodes=23871%7C80
 
+url00 = "http://insight.baidu.com/base/search/rank/list?pageSize=20&source=0&toFixed=1&filterType=1&dateType={}~{}&dimensionid=78&rateType=1000&filterNodes=23871%7C80&callback=_jsonp6g32mtviddc".format()
+url01 = "http://insight.baidu.com/base/search/rank/list?pageSize=20&source=0&toFixed=1&filterType=1&dateType=20180101~20180107&dimensionid=78&rateType=1000&filterNodes=23871%7C80&callback=_jsonp6g32mtviddc"
 url42 = "http://insight.baidu.com/base/search/rank/list?pageSize=20&source=0&toFixed=1&filterType=1&dateType=20181015~20181021&dimensionid=78&rateType=1000&filterNodes=23871%7C80&callback=_jsonp6g32mtviddc"
 url43 = "http://insight.baidu.com/base/search/rank/list?pageSize=20&source=0&toFixed=1&filterType=1&dateType=20181022~20181028&dimensionid=78&rateType=1000&filterNodes=23871%7C80&callback=_jsonp6g32mtviddc"
 url44 = "http://insight.baidu.com/base/search/rank/list?pageSize=20&source=0&toFixed=1&filterType=1&dateType=20181029~20181104&dimensionid=78&rateType=1000&filterNodes=23871%7C80&callback=_jsonp6g32mtviddc"
@@ -69,7 +70,7 @@ def getOrigin(url):
         print(i, v, r)
 
 
-getOrigin(url42)
+getOrigin(url01)
 print('*'*50)
 getOrigin(url43)
 print('*'*50)
