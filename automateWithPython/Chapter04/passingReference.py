e97b0ced260bbc8c0f8605d6fdeaeb9c7d39684d
liyunyun <liyunyun@ccmtjf.com>
2019-10-19 20:56:13 +0800

    test

--- automateWithPython/Chapter04/passingReference.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-def eggs(someParameter):
-    someParameter.append('Hello')
-
-spam = [1, 2, 3]
-eggs(spam)
-print(spam)
--- automateWithPython/Chapter15/timeDelta.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-import datetime
-dt = datetime.datetime(2010, 9, 3)
-print(dt)
-
-timedelta = datetime.timedelta(days=1389)
